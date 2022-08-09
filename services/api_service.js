9448b370d27b9fa4a0461ab3579e58ff991946ff
EngimaticThrakon <gbrown92799@gmail.com>
2022-08-10 00:41:57 +0100

    Finalized base functions for api service

--- services/api_service.js
+++ services/api_service.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const axios = require('axios');
 
 module.exports = class ApiService {
     constructor(url, port) {
@@ -10,55 +10,74 @@ module.exports = class ApiService {
         return performGet('/api/Units/Connect', [ serialNumber ]);
     }
 
-    performPost(apiEndpoint, data) {
-        const postData = JSON.stringify(data);
-        const options = {
-            hostname: this.baseUrl,
-            port: this.basePort,
-            path: apiEndpoint, //(i.e. /api/Units/connect)
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': Buffer.byteLength(postData)
+    // Base function to get data from a GET endpoint
+    //      apiEndpoint: string in the format "api/<controller>/<endpoint-before-values>"
+    //      options: array of values to be passed to endpoint in order that they will be received
+    //
+    // Returns an object with 'success' = true and the response data if the request is successful,
+    //      'success' = false and error value if the request failed
+    async performGet(apiEndpoint, options) {
+        try {
+            var response = await axios.get(`${this.baseUrl}:${this.basePort}/${apiEndpoint}/${this.assembleOptions(options)}`);
+
+            if (response.status == 200) {
+                return { success: true, data: response.data };
+            } else if (response.status == 204) {
+                return { success: true, data: response.status.toString() };
             }
-        };
 
-        return new Promise((resolve, reject) => {
-            const req = http.request(options, (res) => {
-                res.setEncoding('utf8');
-                res.on('data', (data) => {
-                    resolve(data);
-                });
-                res.on('end', () => {
-                    console.log("Post Complete");
-                });
-            });
-
-            req.on('error', (e) => {
-                reject(e);;
-            });
-
-            req.write(postData);
-            req.end();
-        })
+            return { success: false, data: `Unknown status code: ${response.status}` };
+        } catch (e) {
+            return { success: false, data: e.message };
+        }
     }
 
-    performGet(apiEndpoint, values) {
-        return new Promise((resolve, reject) => {
-            const req = http.get(`${this.baseUrl}:${this.basePort}/${apiEndpoint}/${this.assembleOptions(values)}`, res => {
-                res.on('data', data => {
-                    resolve(data);
-                })
-            });
-
-            req.on('error', error => {
-                reject(error);
-            });
-
-            req.end();
-        })
+    // Base function to perform action on PUT endpoint
+    //      apiEndpoint: string in the format "api/<controller>/<endpoint>"
+    //      options: object to be passed to endpoint
+    //
+    // Returns an object with 'success' = true and the response data if the request is successful,
+    //      'success' = false and error value if the request failed
+    async performPut(apiEndpoint, options) {
+        try {
+            var response = await axios.put(`${this.baseUrl}:${this.basePort}/${apiEndpoint}`, options);
+            
+            if (response.status == 200) {
+                return { success: true, data: response.data };
+            } else if (response.status == 204) {
+                return { success: true, data: response.status.toString() };
+            }
+            
+            return { success: false, data: `Unknown status code: ${response.status}` };
+        } catch (e) {
+            return { success: false, data: e.message };
+        }
     }
 
+    // Base function to perform action on POST endpoint
+    //      apiEndpoint: string in the format "api/<controller>/<endpoint>"
+    //      options: object to be passed to endpoint
+    //
+    // Returns an object with 'success' = true and the response data if the request is successful,
+    //      'success' = false and error value if the request failed
+    async performPost(apiEndpoint, options) {
+        try {
+            var response = await axios.post(`${this.baseUrl}:${this.basePort}/${apiEndpoint}`, options);
+            
+            if (response.status == 200) {
+                return { success: true, data: response.data };
+            } else if (response.status == 204) {
+                return { success: true, data: response.status.toString() };
+            }
+            
+            return { success: false, data: `Unknown status code: ${response.status}` };
+        } catch (e) {
+            return { success: false, data: e.message };
+        }
+    }
+
+    // Function to assemble an array of options into a string that can be appended
+    //      to the end of a url for a GET request
     assembleOptions(options) {
         var returnValue = "";
         options.forEach(t => {
