90301e3954e107f908e9b685384a6906c50be0a0
EngimaticThrakon <gbrown92799@gmail.com>
2022-08-09 02:44:16 +0100

    Broke out the signal R connector and the main program

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-const signalR = require("@microsoft/signalr");
+process.env.NODE_ENV = 'config';
 
-var hubConnection = new signalR.HubConnectionBuilder()
-    .withUrl("http://marvin.webredirect.org:52042/agerDeviceHub")
-    .withAutomaticReconnect()
-    .build();
+const { spawn, exec, execSync } = require('child_process');
+const config = require('config');
 
-hubConnection.start().then(t => {
-    hubConnection.send('test', 'testing');
-})
+var deviceInfo = {};
+var connectionInfo = {};
 
-for(var i = 0; i < 1000; i++) {
+deviceInfo.serialNumber = execSync('cat /proc/cpuinfo | grep Serial').toString().split(':')[1].trim();
 
+connectionInfo.url = config.get('serverUrl');
+connectionInfo.port = config.get('serverPort');
+
+const ApiService = require('./services/api_service');
+const SignalRService = require('./services/signalR_service');
+
+// const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+var apiHandler = new ApiService(url, port);
+var signalR = null;
+
+run();
+
+async function run() {
+    deviceInfo.unitId = await apiHandler.connect(deviceInfo.serialNumber);
+
+    signalR = new SignalRService(connectionInfo.url, connectionInfo.port, deviceInfo.unitId);
+
+    while(true) {
+        await delay(10000);
+    }
 }
