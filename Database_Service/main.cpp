07ba29dd301fa47c96719b91202d673c80bbceba
EnigmaticThrakon <gbrown92799@gmail.com>
2022-10-27 22:23:42 +0100

    Compiled quick program to test connection to SQLite database and Redis

--- Database_Service/main.cpp
+++ Database_Service/main.cpp
@@ -1,4 +1,6 @@
 #include <sqlite3.h>
+#include <iostream>
+#include <hiredis/hiredis.h>
 
 // Callback function required by SQLite connector
 int callback(void *NotUsed, int argc, char **argv, char **azColName)
@@ -31,4 +33,51 @@ bool execute(sqlite3 **db, const char *command, std::string successString)
         sqlite3_free(errorMessage);
 
     return success;
+}
+
+int main(int argc, char* argv[])
+{
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+
+    rc = sqlite3_open("test.db", &db);
+
+    if(rc) 
+    {
+        std::cout << "Can't open database: " << sqlite3_errmsg(db);
+        exit(1);
+    }
+    else
+    {
+        std::cout << "Opened database successfully" << std::endl;
+    }
+
+    sqlite3_close(db);
+
+    redisContext *c;
+    const char *hostname = "127.0.0.1";
+    int port = 6379;
+
+    c = redisConnect(hostname, port);
+
+    if(c == NULL || c->err)
+    {
+        if(c)
+        {
+            std::cout << "Connection Error: " << c->errstr;
+            redisFree(c);
+        }
+        else
+        {
+            std::cout << "Connection Errror: Can't Allocate Redis Context";
+        }
+
+        exit(1);
+    }
+
+    std::cout << "Successfully connected to Redis";
+    redisFree(c);
+
+    return 0;
 }
