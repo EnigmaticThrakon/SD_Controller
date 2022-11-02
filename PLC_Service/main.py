d79266696a1579e02d9c173422125f30eb03e510
EnigmaticThrakon <gbrown92799@gmail.com>
2022-11-02 05:50:28 +0000

    Added comments and updated exit codes

--- PLC_Service/main.py
+++ PLC_Service/main.py
@@ -4,16 +4,22 @@ import random
 from time import sleep
 import redis
 
+# Main function to handle the operations of the service
 def main():
+
+    # Variable to hold the service name if needed for logging
     service_name = "plc_service"
 
+    # Declaring the conection to the Redis database
     redis_context = redis.Redis(decode_responses=True, charset="utf-8")
 
+    # Testing the connection to Redis and stopping the service if a connection
+    #       cannot be established
     try:
         redis_context.ping()
     except Exception as ex:
         print("Error connecting to Redis " + str(ex))
-        quit()
+        quit(1)
 
     redis_context.rpush("logs:" + service_name, "Successfully Connected to Redis")
 
@@ -31,7 +37,7 @@ def main():
         simulated_data_string = json.dumps(simulated_data, default=str)
         print(simulated_data_string)
 
-        acquisition_started = int(redis_context.get("acquisition:started"))
+        acquisition_started = int(redis_context.get('acquisition:started')) if redis_context.get('acquisition:started') is not None else 0
         if acquisition_started is not None and acquisition_started == 1:
             print(simulated_data_string + " -> Redis")
             redis_context.publish("live:data", simulated_data_string)
@@ -42,6 +48,7 @@ def main():
 
     redis_context.rpush("logs:" + service_name, "Shutting Down Service")
     redis_context.close()
+    quit(0)
 
 if __name__ == '__main__':
     main()
