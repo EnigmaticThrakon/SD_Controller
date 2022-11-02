856e594ca358c8d3e248ca13f8a52f5901f9878e
EnigmaticThrakon <gbrown92799@gmail.com>
2022-11-02 20:29:11 +0000

    Added sending a message to the PLC based on redis key

--- PLC_Service/main.py
+++ PLC_Service/main.py
@@ -1,8 +1,15 @@
 from datetime import datetime
+from pylogix import PLC
 import json
 import random
 from time import sleep
 import redis
+
+def writePLC(data):
+    comm = PLC('10.1.1.10')
+
+    if data is not None and data["data"] is not None:
+        ret = comm.Write('DiskEnable', int(data["data"]))
 
 # Main function to handle the operations of the service
 def main():
@@ -25,14 +32,22 @@ def main():
     #       to Redis
     redis_context.rpush("logs:" + service_name, "Successfully Connected to Redis")
 
+    action_listener = redis_context.pubsub()
+    action_listener.subscribe(**{'write:plc':writePLC})
+    action_listener_th = action_listener.run_in_thread(sleep_time=1, daemon=True)
+
     # Variable to hold whether or not the service should be stopped
     stop_service = 0
+
+    comm = PLC('10.1.1.10')
 
     # Starting value of the weight (used for simulating)
     weight = 50
 
     # While loop to continue running while the service is running
     while stop_service != 1:
+
+        ret = comm.Read('L00S02I00')
 
         # Data blob that is composed into an object before being sent into Redis
         simulated_data = {
@@ -41,7 +56,7 @@ def main():
                 "humidity": random.randrange(60, 90),
                 "airFlow": random.randrange(16, 66) / 10,
                 "weight": weight,
-                "door": random.randrange(0, 1)}
+                "door": ret.Value if ret.Value is not None else 0}
 
         # Data blob is serialized into a JSON string
         simulated_data_string = json.dumps(simulated_data, default=str)
@@ -61,7 +76,9 @@ def main():
         stop_service = int(redis_context.get('shutdown')) if redis_context.get('shutdown') is not None else 0
 
         # Letting the loop sleep for a second (might be removed after no longer simulating)
-        sleep(1)
+        sleep(0.1)
+
+    action_listener.unsubscribe('write:plc')
 
     # Pushing another log to show the service is shutting down and closing the connection to Redis
     redis_context.rpush("logs:" + service_name, "Shutting Down Service")
